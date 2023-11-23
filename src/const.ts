45d1a210eac5adf5a059effbc36c257c9d9f68a0
cberto <bertolanicami@gmail.com>
2023-11-22 22:43:41 -0300

    Revert "archivos para deploy"
    This reverts commit 4785986dd91b8827076202e9bbe50c514f6c7b4a.

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,4 @@
-//export const API_URL = import.meta.env.VITE_BACKEND_URL + '/api'
-export const API_URL = '/api'
+export const API_URL = import.meta.env.VITE_BACKEND_URL + '/api'
 
 //types
 export type inputChangeEvent = React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
